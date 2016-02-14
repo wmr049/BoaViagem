01c613cce3f508fbc2af437435f3ca2173f7fe36
mreis <wmr049@gmail.com>
2016-02-14 08:50:53 -0200

    [Livro Android] - Cap03 Criação de Menu de Contexto

--- app/src/main/java/br/com/casadocodigo/boaviagem/GastoListActivity.java
+++ app/src/main/java/br/com/casadocodigo/boaviagem/GastoListActivity.java
@@ -2,6 +2,9 @@ package br.com.casadocodigo.boaviagem;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.SimpleAdapter;
@@ -39,6 +42,8 @@ public class GastoListActivity extends ListActivity implements AdapterView.OnIte
 
         setListAdapter(adapter);
         getListView().setOnItemClickListener(this);
+
+        registerForContextMenu(getListView());
     }
 
     @Override
@@ -107,6 +112,27 @@ public class GastoListActivity extends ListActivity implements AdapterView.OnIte
             }
             return false;
         }
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.gasto_menu, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.remover) {
+            AdapterView.AdapterContextMenuInfo info =
+                    (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+            gastos.remove(info.position);
+            getListView().invalidateViews();
+            dataAnterior = "";
+
+            return true;
+        }
+        return super.onContextItemSelected(item);
     }
 
 }
