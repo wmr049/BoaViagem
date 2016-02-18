e1dbe7d0957d9ddc5c77ededfb50f4e11cecef00
mreis <wmr049@gmail.com>
2016-02-18 07:10:00 -0200

    [Livro Android] - 4.6 Dicas e boas práticas ao trabalhar com
    banco de dados no Android

--- app/src/main/java/br/com/casadocodigo/boaviagem/DatabaseHelper.java
+++ app/src/main/java/br/com/casadocodigo/boaviagem/DatabaseHelper.java
@@ -12,6 +12,38 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String BANCO_DADOS = "BoaViagem";
     private static int VERSAO = 1;
 
+    public static class Viagem {
+        public static final String TABELA = "viagem";
+        public static final String _ID = "_id";
+        public static final String DESTINO = "destino";
+        public static final String DATA_CHEGADA = "data_chegada";
+        public static final String DATA_SAIDA = "data_saida";
+        public static final String ORCAMENTO = "orcamento";
+        public static final String QUANTIDADE_PESSOAS = "quantidade_pessoas";
+        public static final String TIPO_VIAGEM = "tipo_viagem";
+
+        public static final String[] COLUNAS = new String[]{
+                _ID, DESTINO, DATA_CHEGADA, DATA_SAIDA,
+                TIPO_VIAGEM, ORCAMENTO, QUANTIDADE_PESSOAS };
+
+    }
+
+    public static class Gasto {
+        public static final String TABELA = "gasto";
+        public static final String _ID = "_id";
+        public static final String VIAGEM_ID = "viagem_id";
+        public static final String CATEGORIA = "categoria";
+        public static final String DATA = "data";
+        public static final String DESCRICAO = "descricao";
+        public static final String VALOR = "valor";
+        public static final String LOCAL = "local";
+
+        public static final String[] COLUNAS = new String[]{
+                _ID, VIAGEM_ID, CATEGORIA, DATA, DESCRICAO,
+                VALOR, LOCAL
+        };
+    }
+
 
     public DatabaseHelper(Context context) {
         super(context, BANCO_DADOS, null, VERSAO);
@@ -34,4 +66,6 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("ALTER TABLE gasto ADD COLUMN pessoa TEXT");
     }
+
+
 }
---
+++ app/src/main/java/br/com/casadocodigo/boaviagem/domain/Viagem.java
@@ -0,0 +1,84 @@
+package br.com.casadocodigo.boaviagem.domain;
+
+import java.util.Date;
+
+/**
+ * Created by mreis on 18/02/2016.
+ */
+public class Viagem {
+    private Long id;
+    private String destino;
+    private Integer tipoViagem;
+    private Date dataChegada;
+    private Date dataSaida;
+    private Double orcamento;
+    private Integer quantidadePessoas;
+    public Viagem(){}
+    public Viagem(Long id, String destino, Integer tipoViagem,
+                  Date dataChegada, Date dataSaida, Double orcamento,
+                  Integer quantidadePessoas) {
+        this.id = id;
+        this.destino = destino;
+        this.tipoViagem = tipoViagem;
+        this.dataChegada = dataChegada;
+        this.dataSaida = dataSaida;
+        this.orcamento = orcamento;
+        this.quantidadePessoas = quantidadePessoas;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public Integer getTipoViagem() {
+        return tipoViagem;
+    }
+
+    public void setTipoViagem(Integer tipoViagem) {
+        this.tipoViagem = tipoViagem;
+    }
+
+    public Date getDataChegada() {
+        return dataChegada;
+    }
+
+    public void setDataChegada(Date dataChegada) {
+        this.dataChegada = dataChegada;
+    }
+
+    public Date getDataSaida() {
+        return dataSaida;
+    }
+
+    public void setDataSaida(Date dataSaida) {
+        this.dataSaida = dataSaida;
+    }
+
+    public Double getOrcamento() {
+        return orcamento;
+    }
+
+    public void setOrcamento(Double orcamento) {
+        this.orcamento = orcamento;
+    }
+
+    public Integer getQuantidadePessoas() {
+        return quantidadePessoas;
+    }
+
+    public void setQuantidadePessoas(Integer quantidadePessoas) {
+        this.quantidadePessoas = quantidadePessoas;
+    }
+}
